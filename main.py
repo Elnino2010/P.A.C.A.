bf88db13cdbe54ba659bf3d172aebf29b37d94c8
ElNino2010 <rsnicolas0@gmail.com>
2026-07-03 17:18:05 +0200

    Empieza el proyecto PACA.

---
+++ main.py
@@ -0,0 +1,46 @@
+#Importamos las librerías necesarias para el funcionamiento
+import ollama
+import time
+#Importamos funciones de otros bloques de código
+#from archivo import funcion
+
+#DEFINIMOS VARIABLES DEL PROYECTO
+modelo = 'gemma4:e4b'
+pregunta = ""
+mensaje = []
+respuesta = ""
+
+def chat():
+    print("\n" + "="*40)
+    print("Hola señor, estoy listo para servirle")
+
+    while True:
+        #Le pedimos un mensaje al usuario
+        pregunta = str(input("Usuario: "))
+
+        #Esto evita el error de darle a enter por accidente
+        if pregunta == "":
+            continue
+
+        #Salir del asistente
+        if pregunta.lower() in ['salir', 'apágate', 'desconectar', 'adios']:
+            print("Entendido Señor. Que tenga un buen día.")
+            break 
+        
+        #Le indicamos a la IA como debe responder
+        mensaje.append({"role": "user",
+                         "content": f"{pregunta}"
+                         })
+
+        try:
+            #Aquí se lanza el mensaje
+            respuesta = ollama.chat(model=modelo, messages=mensaje)
+            
+            #Imprimimos el mensaje
+            print(respuesta.message.content)
+        
+        #Comprobamos si hay error y lo mandamos
+        except Exception as error:
+            print(f"El código falló debido a: {error}")
+if __name__ == "__main__":
+    chat()
